19a8d768cca844f876ff237a6f3b173b7dd6614d
ezequias-dev <ezequiasd30@gmail.com>
2026-04-27 08:02:59 -0300

    feat: add day 4 ex2

--- src/com/ezequias/orderflow/day04/Product.java
+++ src/com/ezequias/orderflow/day04/Product.java
@@ -4,4 +4,10 @@ public class Product {
     String name;
     double price;
     int stockQuantity;
+
+    public Product(String name, double price, int stockQuantity) {
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
 }
--- src/com/ezequias/orderflow/day04/ProductTest.java
+++ src/com/ezequias/orderflow/day04/ProductTest.java
@@ -2,11 +2,7 @@ package com.ezequias.orderflow.day04;
 
 public class ProductTest {
     public static void main(String[] args) {
-        Product product = new Product();
-
-        product.name = "Mouse";
-        product.price = 150.0;
-        product.stockQuantity = 3;
+        Product product = new Product("Keyboard", 150.0, 3);
 
         System.out.println("Name: " + product.name);
         System.out.printf("Price: $%.2f%n", product.price);
